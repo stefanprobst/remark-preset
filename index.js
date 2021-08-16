07264042e65817a0e99ddf6d6f3ca6cd52ce85b8
Stefan Probst <stefanprobst@kolabnow.com>
2021-08-16 16:12:09 +0200

    build: update unified

--- index.js
+++ index.js
@@ -1,4 +1,43 @@
-module.exports = {
+import remarkPresetLintMarkdownStyleGuide from 'remark-preset-lint-markdown-style-guide'
+import remarkPresetLintRecommended from 'remark-preset-lint-recommended'
+import remarkLintEmphasisMarker from 'remark-lint-emphasis-marker'
+import remarkLintListItemIndent from 'remark-lint-list-item-indent'
+import remarkLintListItemSpacing from 'remark-lint-list-item-spacing'
+import remarkLintFirstHeadingLevel from 'remark-lint-first-heading-level'
+import remarkLintNoAutoLinks from '@stefanprobst/remark-lint-no-auto-links'
+import remarkLintNoAutoLinkWithoutProtocol from 'remark-lint-no-auto-link-without-protocol'
+import remarkLintNoEmptyUrl from 'remark-lint-no-empty-url'
+import remarkLintValidateLinks from 'remark-validate-links'
+import remarkLintFileExtension from 'remark-lint-file-extension'
+import remarkLintNoFileNameArticles from 'remark-lint-no-file-name-articles'
+import remarkLintNoFileNameIrregularCharacters from 'remark-lint-no-file-name-irregular-characters'
+import remarkLintBlockquoteIndentation from 'remark-lint-blockquote-indentation'
+import remarkLintCodeBlockStyle from 'remark-lint-code-block-style'
+import remarkLintDefinitionCase from 'remark-lint-definition-case'
+import remarkLintDefinitionSpacing from 'remark-lint-definition-spacing'
+import remarkLintFencedCodeMarker from 'remark-lint-fenced-code-marker'
+import remarkLintFinalNewline from 'remark-lint-final-newline'
+import remarkLintHardBreakSpaces from 'remark-lint-hard-break-spaces'
+import remarkLintHeadingStyle from 'remark-lint-heading-style'
+import remarkLintLinkTitleStyle from 'remark-lint-link-title-style'
+import remarkLintListItemBulletIndent from 'remark-lint-list-item-bullet-indent'
+import remarkLintListItemContentIndent from 'remark-lint-list-item-content-indent'
+import remarkLintMaximumLineLength from 'remark-lint-maximum-line-length'
+import remarkLintNoBlockquoteWithoutMarker from 'remark-lint-no-blockquote-without-marker'
+import remarkLintNoConsecutiveBlankLines from 'remark-lint-no-consecutive-blank-lines'
+import remarkLintNoHeadingContentIndent from 'remark-lint-no-heading-content-indent'
+import remarkLintNoInlinePadding from 'remark-lint-no-inline-padding'
+import remarkLintNoTableIndentation from 'remark-lint-no-table-indentation'
+import remarkLintOrderedListMarkerStyle from 'remark-lint-ordered-list-marker-style'
+import remarkLintOrderedListMarkerValue from 'remark-lint-ordered-list-marker-value'
+import remarkLintRuleStyle from 'remark-lint-rule-style'
+import remarkLintStrongMarker from 'remark-lint-strong-marker'
+import remarkLintTableCellPadding from 'remark-lint-table-cell-padding'
+import remarkLintTablePipeAlignment from 'remark-lint-table-pipe-alignment'
+import remarkLintTablePipes from 'remark-lint-table-pipes'
+import remarkLintUnorderedListMarkerStyle from 'remark-lint-unordered-list-marker-style'
+
+export default {
   settings: {
     bullet: '-',
     emphasis: '_',
@@ -10,47 +49,47 @@ module.exports = {
     strong: '*',
   },
   plugins: [
-    require('remark-preset-lint-markdown-style-guide'),
-    require('remark-preset-lint-recommended'),
-    [require('remark-lint-emphasis-marker'), '_'],
-    [require('remark-lint-list-item-indent'), 'space'],
-    [require('remark-lint-list-item-spacing'), { checkBlanks: true }],
-    [require('remark-lint-ordered-list-marker-value'), 'ordered'],
-    [require('remark-lint-first-heading-level'), 2],
-    require('@stefanprobst/remark-lint-no-auto-links'),
-    [require('remark-lint-no-auto-link-without-protocol'), false],
-    require('remark-lint-no-empty-url'),
-    require('remark-validate-links'),
-    [require('remark-lint-file-extension'), false],
-    [require('remark-lint-no-file-name-articles'), false],
-    [require('remark-lint-no-file-name-irregular-characters'), false],
-    [require('remark-lint-blockquote-indentation'), false],
-    [require('remark-lint-code-block-style'), false],
-    [require('remark-lint-definition-case'), false],
-    [require('remark-lint-definition-spacing'), false],
-    [require('remark-lint-emphasis-marker'), false],
-    [require('remark-lint-fenced-code-marker'), false],
-    [require('remark-lint-final-newline'), false],
-    [require('remark-lint-hard-break-spaces'), false],
-    [require('remark-lint-heading-style'), false],
-    [require('remark-lint-link-title-style'), false],
-    [require('remark-lint-list-item-bullet-indent'), false],
-    [require('remark-lint-list-item-content-indent'), false],
-    [require('remark-lint-list-item-indent'), false],
-    [require('remark-lint-list-item-spacing'), false],
-    [require('remark-lint-maximum-line-length'), false],
-    [require('remark-lint-no-blockquote-without-marker'), false],
-    [require('remark-lint-no-consecutive-blank-lines'), false],
-    [require('remark-lint-no-heading-content-indent'), false],
-    [require('remark-lint-no-inline-padding'), false],
-    [require('remark-lint-no-table-indentation'), false],
-    [require('remark-lint-ordered-list-marker-style'), false],
-    [require('remark-lint-ordered-list-marker-value'), false],
-    [require('remark-lint-rule-style'), false],
-    [require('remark-lint-strong-marker'), false],
-    [require('remark-lint-table-cell-padding'), false],
-    [require('remark-lint-table-pipe-alignment'), false],
-    [require('remark-lint-table-pipes'), false],
-    [require('remark-lint-unordered-list-marker-style'), false],
+    remarkPresetLintMarkdownStyleGuide,
+    remarkPresetLintRecommended,
+    // [remarkLintEmphasisMarker, '_'],
+    // [remarkLintListItemIndent, 'space'],
+    // [remarkLintListItemSpacing, { checkBlanks: true }],
+    [remarkLintOrderedListMarkerValue, 'ordered'],
+    [remarkLintFirstHeadingLevel, 2],
+    remarkLintNoAutoLinks,
+    [remarkLintNoAutoLinkWithoutProtocol, false],
+    remarkLintNoEmptyUrl,
+    remarkLintValidateLinks,
+    [remarkLintFileExtension, false],
+    [remarkLintNoFileNameArticles, false],
+    [remarkLintNoFileNameIrregularCharacters, false],
+    [remarkLintBlockquoteIndentation, false],
+    [remarkLintCodeBlockStyle, false],
+    [remarkLintDefinitionCase, false],
+    [remarkLintDefinitionSpacing, false],
+    [remarkLintEmphasisMarker, false],
+    [remarkLintFencedCodeMarker, false],
+    [remarkLintFinalNewline, false],
+    [remarkLintHardBreakSpaces, false],
+    [remarkLintHeadingStyle, false],
+    [remarkLintLinkTitleStyle, false],
+    [remarkLintListItemBulletIndent, false],
+    [remarkLintListItemContentIndent, false],
+    [remarkLintListItemIndent, false],
+    [remarkLintListItemSpacing, false],
+    [remarkLintMaximumLineLength, false],
+    [remarkLintNoBlockquoteWithoutMarker, false],
+    [remarkLintNoConsecutiveBlankLines, false],
+    [remarkLintNoHeadingContentIndent, false],
+    [remarkLintNoInlinePadding, false],
+    [remarkLintNoTableIndentation, false],
+    [remarkLintOrderedListMarkerStyle, false],
+    // [remarkLintOrderedListMarkerValue, false],
+    [remarkLintRuleStyle, false],
+    [remarkLintStrongMarker, false],
+    [remarkLintTableCellPadding, false],
+    [remarkLintTablePipeAlignment, false],
+    [remarkLintTablePipes, false],
+    [remarkLintUnorderedListMarkerStyle, false],
   ],
 }
